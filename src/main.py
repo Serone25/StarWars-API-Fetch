79e3282f896e372c64afac6b288e4b316ef74f60
Serone25 <fran19842010@live.com>
2023-01-23 16:36:55 +0000

    First Commit

--- src/main.py
+++ src/main.py
@@ -8,8 +8,9 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet
 #from models import Person
+import json
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -19,6 +20,51 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
+people=[
+    {
+    "id": "0",
+    "name": "Luke Skywalker",
+	"height": "172",
+	"mass": "77",
+	"hair_color": "blond",
+	"skin_color": "fair",
+	"eye_color": "blue"
+    },
+    {
+    "id":"1",
+    "name": "C-3PO",
+	"height": "167",
+	"mass": "75",
+	"hair_color": "n/a",
+	"skin_color": "gold",
+	"eye_color": "yellow"
+    }
+]
+
+planets = [
+    {
+    "id":"0",
+    "name": "Tatooine",
+	"rotation_period": "23",
+	"orbital_period": "304",
+	"diameter": "10465",
+	"climate": "arid",
+	"gravity": "1 standard",
+	"terrain": "desert"
+    },
+    {
+    "id":"1",
+    "name": "Alderaan",
+	"rotation_period": "24",
+	"orbital_period": "364",
+	"diameter": "12500",
+	"climate": "temperate",
+	"gravity": "1 standard",
+	"terrain": "grasslands, mountains"
+    }
+]
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -38,6 +84,57 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/people',methods=['GET'])
+def handle_characters():
+    
+    return jsonify(people), 200
+
+@app.route('/people/<int:people_id>',methods=['GET'])
+def handle_character(people_id):
+    person = people[people_id]
+    person = filter(lambda element: element["id"] == people_id, people)
+    return jsonify(person), 200
+
+#Cojo los datos del array planets
+@app.route('/planets',methods=['GET'])
+def handle_planets():
+    
+    return jsonify(planets), 200
+
+#Cojo los datos de la base de datos en models.py
+@app.route('/planet/<id>',methods=['GET'])
+def handle_planet():
+    #planet = Planet.query.get(id)
+    #print(planet.serialize())       #Llamo al metodo serialize para ver el objeto
+
+    planet = Planet.query.filter_by(id=id).one()
+    planet = planet.serialize()
+    return jsonify(planet), 200
+
+#Cojo todos los datos de la base de datos
+@app.route('/allplanets',methods=['GET'])
+def get_all_planets():
+    planets = Planet.query.all()            #Creo un array y le meto todos los objetos
+    result = []                             #Array vacío
+    for planet in planets:                  #Por cada elemento en el array planets
+        result.append(planets.serialize())  #meteme en el array vacio el objeto serializado
+
+    #mismo resultado escrito de otras dos maneras:
+    #result = [planet.serialize() for planet in planets]
+    #result = list(map(lambda planet : planet.serialize(),planets))
+
+    return jsonify(result), 200
+
+@app.route('/planet',methods=['POST'])
+def create_planet():
+    data = request.data
+    #print(data)
+    data = json.loads(data)
+    planet = Planet(name = data["name"], description = data["description"])
+    db.session.add(planet)
+    db.session.commit()
+    return jsonify(planet), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -8,12 +8,31 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    #Representacion en string del objeto en el que estamos con uno de los atributos, en este caso el username
+    #Representación del objeto con un solo valor
     def __repr__(self):
         return '<User %r>' % self.username
 
+    #Cuando iteramos un objeto, lo que le vamos a pedir a la base de datos que muestre u obtenga
+    # Representación total del objeto, por eso no se pide el password, para que no se vea 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(250), unique=False, nullable=False)
+
+    def __repr__(self):
+        return  self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.email,
+            "description": self.description
         }
